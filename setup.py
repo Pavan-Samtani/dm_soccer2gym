c41457099c6199b2984271b782f262bcba6699f9
pavan-samtani <pavan3398@gmail.com>
2019-11-22 20:54:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name = "dm_control2gym",
+    name = "dm_soccer2gym",
     version = "0.0.4",
     author = "Martin Seiler",
-    description = ("OpenAI Gym Wrapper for DeepMind Control Suite"),
+    description = ("OpenAI Gym Wrapper for DeepMind Control Soccer"),
     license = "",
     keywords = "openai gym deepmind wrapper",
     packages=find_packages(),
